f1200d1a3c7d2a1dacdfd58fe8e0bc9f3e9bd5a4
Nakul Desai <nakuldesai2006@gmail.com>
2024-06-23 15:51:02 +0530

    flash

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-
-# Flask Instance
+# Create a Flask Instance
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
-
-# Create Form Class
+# Create a Form Class
 class NamerForm(FlaskForm):
-	name = StringField("What's Your Name?", validators=[DataRequired()])
+	name = StringField("What's Your Name", validators=[DataRequired()])
 	submit = SubmitField("Submit")
 
-	## Fields
 	# BooleanField
 	# DateField
 	# DateTimeField
 	# DecimalField
 	# FileField
 	# HiddenField
-	# MultipleFIeld
-	# FIeldList
+	# MultipleField
+	# FieldList
 	# FloatField
-	# FormFIeld
+	# FormField
 	# IntegerField
 	# PasswordField
 	# RadioField
@@ -32,7 +30,7 @@ class NamerForm(FlaskForm):
 	# StringField
 	# TextAreaField
 
-	##Validators
+	## Validators
 	# DataRequired
 	# Email
 	# EqualTo
@@ -48,54 +46,50 @@ class NamerForm(FlaskForm):
 	# AnyOf
 	# NoneOf
 
-# Filters:-
-# 1.safe
-# 2.capitalise
-# 3.lower
-# 4.upper
-# 5.title
-# 6.trim
-# 7.striptags
-
-
-# Localhost
+# Create a route decorator
 @app.route('/')
+#def index():
+#	return "<h1>Hello World!</h1>"
+# FILTERS!!!
+#safe
+#capitalize
+#lower
+#upper
+#title
+#trim
+#striptags
 def index():
-	first_name = "Nakul"
+	first_name = "John"
 	stuff = "This is bold text"
-
 	favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
 	return render_template("index.html", 
 		first_name=first_name,
 		stuff=stuff,
 		favorite_pizza = favorite_pizza)
-
 # localhost:5000/user/John
 @app.route('/user/<name>')
 def user(name):
 	return render_template("user.html", user_name=name)
-
-
-# Create custom Error Pages
-
-# Invalid URL Error
+# Create Custom Error Pages
+# Invalid URL
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
-
+	return render_template("404.html"), 404
 # Internal Server Error
 @app.errorhandler(500)
 def page_not_found(e):
-    return render_template('500.html'), 500
-
-# localhost:5000/user/John
-@app.route('/name', methods=["POST", "GET"])
+	return render_template("500.html"), 500
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
 def name():
 	name = None
 	form = NamerForm()
+	# Validate Form
 	if form.validate_on_submit():
 		name = form.name.data
 		form.name.data = ''
-	return render_template("name.html",
-							name=name,
-							form=form)
+		flash("Form Submitted Successfully!")
+
+	return render_template("name.html", 
+		name = name,
+		form = form)
