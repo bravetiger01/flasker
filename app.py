e63aaea531be48af2f5f3946f4d237565f8f9eae
Nakul Desai <nakuldesai2006@gmail.com>
2024-06-24 17:17:13 +0530

    minorerror

--- app.py
+++ app.py
@@ -23,7 +23,16 @@ app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Json Thing
+@app.route('/date')
+def get_current_date():
+	favorite_pizza = {
+		"John": "Pepperoni",
+		"Mary": "Mushroom",
+		"David": "Cheese",
 
+	}
+	return favorite_pizza
 
 
 
@@ -36,7 +45,7 @@ class Users(db.Model):
 	field = db.Column(db.String(120))
 	date_added = db.Column(db.DateTime, default=datetime.utcnow)
 	# Do some password stuff!
-	password_hash = db.Column(db.String(128))
+	password_hash = db.Column(db.String(200))
 
 	@property
 	def password(self):
@@ -52,6 +61,14 @@ class Users(db.Model):
 	# Create A String
 	def __repr__(self):
 		return '<Name %r>' % self.name
+	
+class Blogs(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(255), nullable=False)
+	content = db.Column(db.Text, nullable=False)
+	author = db.Column(db.String(255), nullable=False)
+	date_posted = db.Column(db.DateTime, default=datetime.now())
+	slug = db.Column(db.String(255), nullable=False)
 
 @app.route('/delete/<int:id>')
 def delete(id):
