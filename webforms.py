828f2f6ac31f2263a279e97863ed088fec7a4155
Nakul Desai <125589034+bravetiger01@users.noreply.github.com>
2024-09-01 15:10:19 +0530

    minorchange

--- webforms.py
+++ webforms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired, EqualTo
-from wtforms.widgets import TextArea
 from flask_ckeditor import CKEditorField
 from flask_wtf.file import FileField
 
